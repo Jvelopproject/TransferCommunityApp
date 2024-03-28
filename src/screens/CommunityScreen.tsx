1be21d7dc1f3580acaedd2c3bf42625a68c7db02
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-03-29 01:21:26 +0900

    navigation type 지정

--- src/screens/CommunityScreen.tsx
+++ src/screens/CommunityScreen.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Text, View } from "react-native";
 
 const CommunityScreen = () => {
-  return <div>CommunityScreen</div>;
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>CommunityScreen</Text>
+    </View>
+  );
 };
 
 export default CommunityScreen;
