e22dc6413325f98b8a5ec2452266393f19599877
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-05-21 01:07:23 +0900

    axios interceptor 추가

---
+++ src/services/axios_interceptor.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { API_URL } from "../../.env.d";
+
+const instance = axios.create({
+  baseURL: API_URL,
+  timeout: 1000,
+});
+
+// 요청 인터셉터 추가하기
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 추가하기
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
